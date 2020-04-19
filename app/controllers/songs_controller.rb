3196baf9ee43b36fb47dc3dee571bfdfde522de1
Jamal Frank <jamaljfrank@gmail.com>
2020-04-19 15:56:43 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,7 +4,7 @@ class SongsController < ApplicationController
       if @artist = Artist.find_by(id: params[:artist_id])
     @songs = @artist.songs
       else 
-    redirect_to artist_path, alert: "Artist not found."
+    redirect_to artists_path, alert: "Artist not found."
       end 
     else 
       @songs = Song.all 
